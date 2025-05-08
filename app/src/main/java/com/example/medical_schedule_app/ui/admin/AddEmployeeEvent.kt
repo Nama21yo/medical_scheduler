f3f1d15fbef2c4b5072230401ef51302c08195ff
dagmawikk <dagmawikassa1@gmail.com>
2025-05-08 17:18:19 +0300

    Include package directive

--- app/src/main/java/com/example/medical_schedule_app/ui/admin/AddEmployeeEvent.kt
+++ app/src/main/java/com/example/medical_schedule_app/ui/admin/AddEmployeeEvent.kt
@@ -1,3 +1,5 @@
+package com.example.medical_schedule_app.ui.admin
+
 sealed class AddEmployeeEvent {
     data class OnNameChanged(val name: String) : AddEmployeeEvent()
     data class OnEmailChanged(val email: String) : AddEmployeeEvent()
