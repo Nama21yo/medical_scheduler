ceb05c5c4828510aa1b6399a40dceb7171d1ab8e
dagmawikk <dagmawikassa1@gmail.com>
2025-05-08 16:42:05 +0300

    Split fullName to mirror database

--- app/src/main/java/com/example/medical_schedule_app/data/models/requests/AddPatientRequest.kt
+++ app/src/main/java/com/example/medical_schedule_app/data/models/requests/AddPatientRequest.kt
@@ -1,7 +1,8 @@
 package com.example.medical_schedule_app.data.models.requests
 
 data class AddPatientRequest(
-    val fullName: String,
+    val firstName: String,
+    val lastName: String,
     val dob: String, // Consider using a standard date format like "YYYY-MM-DD"
     val email: String?, // Email might be optional
     val address: String?, // Address might be optional
