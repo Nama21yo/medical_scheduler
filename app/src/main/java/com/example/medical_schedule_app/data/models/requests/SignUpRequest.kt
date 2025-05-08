22058919851eb02bfed64a9c192d3d9fd323b5aa
Ai engineer SisayThaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-05-08 19:28:29 +0300

    signup request data model

--- app/src/main/java/com/example/medical_schedule_app/data/models/requests/SignUpRequest.kt
+++ app/src/main/java/com/example/medical_schedule_app/data/models/requests/SignUpRequest.kt
@@ -1,2 +1,14 @@
 package com.example.medical_schedule_app.data.models.requests
 
+data class SignupRequest(
+    val email: String,
+    val name: String,
+    val password: String,
+    val specialty: String? = null
+)
+
+data class SignupBody(
+    val name: String,
+    val password: String,
+    val specialty: String? = null
+)
