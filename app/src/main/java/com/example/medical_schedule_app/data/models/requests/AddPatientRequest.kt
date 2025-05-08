a2bb60f5eac5dab3514d766cee6f5e8a8427e4f6
dagmawikk <dagmawikassa1@gmail.com>
2025-05-08 16:55:44 +0300

    Implement addpatientrequest to mirror db

--- app/src/main/java/com/example/medical_schedule_app/data/models/requests/AddPatientRequest.kt
+++ app/src/main/java/com/example/medical_schedule_app/data/models/requests/AddPatientRequest.kt
@@ -1,10 +1,11 @@
 package com.example.medical_schedule_app.data.models.requests
 
 data class AddPatientRequest(
-    val firstName: String,
-    val lastName: String,
-    val dob: String, // Consider using a standard date format like "YYYY-MM-DD"
+    val first_name: String,
+    val last_name: String,
+    val date_of_birth: String, // Consider using a standard date format like "YYYY-MM-DD"
     val email: String?, // Email might be optional
     val address: String?, // Address might be optional
-    val phone: String
+    val phone: String,
+    val gender: String
 )
