acc9a3efd417117788e5bb49bcde0e4933145f58
Ai engineer SisayThaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-05-08 19:27:14 +0300

    apiservice for signup

--- app/src/main/java/com/example/medical_schedule_app/data/api/ApiService.kt
+++ app/src/main/java/com/example/medical_schedule_app/data/api/ApiService.kt
@@ -2,9 +2,11 @@ package com.example.medical_schedule_app.data.api
 
 import com.example.medical_schedule_app.data.models.User
 import com.example.medical_schedule_app.data.models.requests.LoginRequest
+import com.example.medical_schedule_app.data.models.requests.SignupBody
 import com.example.medical_schedule_app.data.models.requests.UserUpdateRequest
 import com.example.medical_schedule_app.data.models.responses.LoginResponse
 import com.example.medical_schedule_app.data.models.responses.RolesResponse
+import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiService {
@@ -15,14 +17,24 @@ interface ApiService {
     @POST("users/login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
 
-    @POST("users/branches/signup/{email}")
-    suspend fun signupBranch(@Path("email") email: String): LoginResponse
-
     @POST("users/doctors/signup/{email}")
-    suspend fun signupDoctor(@Path("email") email: String): LoginResponse
+    suspend fun signupDoctor(
+        @Path("email") email: String,
+        @Body request: SignupBody
+    ): Response<LoginResponse>
 
     @POST("users/receptionists/signup/{email}")
-    suspend fun signupReceptionist(@Path("email") email: String): LoginResponse
+    suspend fun signupReceptionist(
+        @Path("email") email: String,
+        @Body request: SignupBody
+    ): Response<LoginResponse>
+
+    @POST("users/branches/signup/{email}")
+    suspend fun signupBranch(
+        @Path("email") email: String,
+        @Body request: SignupBody
+    ): Response<LoginResponse>
+
 
     @GET("users/user")
     suspend fun getUserProfile(@Header("Authorization") token: String): User
