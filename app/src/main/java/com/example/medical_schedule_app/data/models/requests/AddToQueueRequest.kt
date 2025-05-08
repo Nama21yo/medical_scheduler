9709a6d8fa810fbb70cbe050feff1c673f958b2c
dagmawikk <dagmawikassa1@gmail.com>
2025-05-08 16:42:37 +0300

    Split fullName to mirror database

--- app/src/main/java/com/example/medical_schedule_app/data/models/requests/AddToQueueRequest.kt
+++ app/src/main/java/com/example/medical_schedule_app/data/models/requests/AddToQueueRequest.kt
@@ -1,4 +1,7 @@
 package com.example.medical_schedule_app.data.models.requests
 
-class AddToQueueRequest {
-}
+data class AddToQueueRequest(
+    val patient_id: Int,
+    val doctor_id: Int,
+    val status: Int
+)
